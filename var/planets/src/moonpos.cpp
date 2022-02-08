a8fa3cd1f40941dfd76c4228d1019bd34a2f37d4
xanthos <xanthos.papanikolaou@gmail.com>
2022-02-08 11:32:03 +0200

    planets

--- var/planets/src/moonpos.cpp
+++ var/planets/src/moonpos.cpp
@@ -32,41 +32,48 @@ int dso::moon_vector(double t, double *rsun) noexcept {
   printf("F  = %15.10e\n", F);
 #endif
 
-  const double c[] = {22640e0, 769e0, -4586e0, 2370e0, -668e0, -412e0, -212e0,
-                      -206e0,  192e0, -165e0,  148e0,  -125e0, -110e0, -55e0};
-
   // lunar longitude
-  const double lambda =
-      L0 + c[0] * sin(l) + c[1] * sin(2e0 * l) + c[2] * sin(l - 2e0 * D) +
-      c[3] * sin(2e0 * D) + c[4] * sin(lp) - c[5] * sin(2e0 * F) +
-      c[6] * sin(2e0 * l - 2e0 * D) - c[7] * sin(l + lp - 2e0 * D) +
-      c[8] * sin(l + 2e0 * D) - c[9] * sin(lp - 2e0 * D) + c[10] * sin(l - lp) -
-      c[11] * sin(D) + c[12] * sin(l + lp) - c[13] * sin(2e0 * F - 2e0 * D);
+  const double dlambda =
+    22640e0 * sin(l) + 769e0 * sin(2e0 * l) - 4586e0 * sin(l - 2e0 * D) +
+    2370e0 * sin(2e0 * D) + -668e0 * sin(lp) - 412e0 * sin(2e0 * F) +
+    -212e0 * sin(2e0 * l - 2e0 * D) - 206e0 * sin(l + lp - 2e0 * D) +
+    192e0 * sin(l + 2e0 * D) - 165e0 * sin(lp - 2e0 * D) + 148e0 * sin(l - lp) -
+    125e0 * sin(D) - 110e0 * sin(l + lp) - 55e0 * sin(2e0 * F - 2e0 * D);
+    printf("dlambda = %15.5f\n", dlambda);
 
-  // lunar latitude
-  const double beta =
-      18520e0 * sin(F + lambda - L0 + 412e0 * sin(2e0 * F) + 541e0 * sin(lp)) -
-      526e0 * sin(F - 2e0 * D) + 44e0 * sin(l + F - 2e0 * D) -
-      31e0 * sin(-l + F - 2e0 * D) - 25e0 * sin(-2e0 * l + F) -
-      23e0 * sin(lp + F - 2e0 * D) + 21e0 * sin(-l + F) +
-      11e0 * sin(-lp + F - 2e0 * D);
+    //const double lambda =
+    //    std::fmod(L0 * iers2010::TURNAS + dlambda, iers2010::TURNAS) *
+    //    iers2010::DAS2R;
+    const double lambda =
+        std::fmod((L0 * iers2010::TURNAS + dlambda) * iers2010::DAS2R, iers2010::DPI);
+    printf("lambda = %15.10f\n", lambda);
 
-  // distance
-  const double r = 385000e0 - 20905e0 * cos(l) - 3699e0 * cos(2e0 * D - l) -
-                   2956e0 * cos(2e0 * D) - 570e0 * cos(2e0 * l) +
-                   246e0 * cos(2e0 * l - 2e0 * D) - 205e0 * cos(lp - 2e0 * D) -
-                   171e0 * cos(l + 2e0 * D) - 152e0 * cos(l + lp - 2e0 * D);
+    // lunar latitude
+    const double beta =
+        18520e0 *
+            sin(F + lambda - L0 + 412e0 * sin(2e0 * F) + 541e0 * sin(lp)) -
+        526e0 * sin(F - 2e0 * D) + 44e0 * sin(l + F - 2e0 * D) -
+        31e0 * sin(-l + F - 2e0 * D) - 25e0 * sin(-2e0 * l + F) -
+        23e0 * sin(lp + F - 2e0 * D) + 21e0 * sin(-l + F) +
+        11e0 * sin(-lp + F - 2e0 * D);
 
-  const double x = r * cos(lambda) * cos(beta);
-  const double y = r * sin(lambda) * cos(beta);
-  const double z = r * sin(beta);
-  constexpr double ecliptic_obliquity = dso::deg2rad<double>(23.43929111);
-  const double C = cos(ecliptic_obliquity);
-  const double S = sin(ecliptic_obliquity);
+    // distance
+    const double r = 385000e0 - 20905e0 * cos(l) - 3699e0 * cos(2e0 * D - l) -
+                     2956e0 * cos(2e0 * D) - 570e0 * cos(2e0 * l) +
+                     246e0 * cos(2e0 * l - 2e0 * D) -
+                     205e0 * cos(lp - 2e0 * D) - 171e0 * cos(l + 2e0 * D) -
+                     152e0 * cos(l + lp - 2e0 * D);
 
-  rsun[0] = x;
-  rsun[1] = C * y + S * z;
-  rsun[2] = -S * y + C * z;
+    const double x = r * cos(lambda) * cos(beta);
+    const double y = r * sin(lambda) * cos(beta);
+    const double z = r * sin(beta);
+    constexpr double ecliptic_obliquity = dso::deg2rad<double>(23.43929111);
+    const double C = cos(ecliptic_obliquity);
+    const double S = sin(ecliptic_obliquity);
 
-  return 0;
+    rsun[0] = x;
+    rsun[1] = C * y + S * z;
+    rsun[2] = -S * y + C * z;
+
+    return 0;
 }
