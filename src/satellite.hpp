c47be8a1e91f8741cc44e61c0c8b595b5e3e3e7b
xanthos <xanthos.papanikolaou@gmail.com>
2022-02-17 02:33:58 +0200

    adding drag initial

--- src/satellite.hpp
+++ src/satellite.hpp
@@ -16,24 +16,26 @@ namespace dso {
   int pos2ad(const Vector3 &pos, double &a, double&d) noexcept;
 
   /// @brief Obtain orbital elements from the state vector.
-  /// Applying this algorithm to orbits around other planets or the sun amounts 
-  /// to defining the frame of reference and substituting the appropriate 
+  /// Applying this algorithm to orbits around other planets or the sun amounts
+  /// to defining the frame of reference and substituting the appropriate
   /// gravitational parameter μ(=GM).
   /// This procedure for calculating the orbital elements is not unique.
-  /// param[in] pos State vector of in-orbit element (satellite) in [m] and 
+  /// param[in] pos State vector of in-orbit element (satellite) in [m] and
   ///               [m/s], in the order: (X, Y, Z, Vx, Vy, Vz)
-  /// param[out] kepler_ele Kerplerian (orbital) elements, in the following 
+  /// param[out] kepler_ele Kerplerian (orbital) elements, in the following
   ///               order (input array must be at least of size 6):
   ///               [0]: magnitude of (specific) angular momentum [m^2 / s]
   ///               [1]: inclination i, [rad], range: [0, π]
-  ///               [2]: right ascension of ascending node Ω, [rad], 
+  ///               [2]: right ascension of ascending node Ω, [rad],
   ///                    range [0,2π]
   ///               [3]: magnitude of eccentricity vector, e
   ///               [4]: argument of perigee, ω, [rad], range [0,2π]
   ///               [5]: true anomaly θ, [rad], range [0,2π]
   /// @param[in] GM gravitational parameter [m^3 / s^2]
-  /// @note If you want the computation to be performed using [km] instead of 
+  /// @note If you want the computation to be performed using [km] instead of
   ///       [m], transform the input quantities accordingly (i.e. state and GM)
+  /// @see Orbital Mechanics for Engineering Students, H. D. Curtis, (Fourth
+  ///      Edition), Chapter 4
   int state2kepler(const double *state, double *kepler_ele,
                    double GM = iers2010::GMe) noexcept;
   
@@ -56,8 +58,25 @@ namespace dso {
   /// @param[in] GM gravitational parameter [m^3 / s^2]
   /// @note If you want the computation to be performed using [km] instead of 
   ///       [m], transform the input quantities accordingly (i.e. state and GM)
+  /// @see Orbital Mechanics for Engineering Students, H. D. Curtis, (Fourth
+  ///      Edition), Chapter 4
   int kepler2state(const double *kepler_ele, double *state,
                       double GM = iers2010::GMe) noexcept;
+
+  /// @brief Compute the perturbing acceleration due to the atmospheric drag.
+  /// @param[in] r  Satellite position vector in the inertial system [m]
+  /// @param[in] v  Satellite velocity vector in the inertial system [m/s]
+  /// @param[in] Area Cross-section [m^2]
+  /// @param[in] mass Spacecraft mass [kg]
+  /// @param[in] CD   Drag coefficient
+  /// @param[in] atmdens Atmospheric density
+  /// @return Acceleration (a=d^2r/dt^2) [m/s^2]
+  /// @see Orbital Mechanics for Engineering Students, H. D. Curtis, (Fourth
+  ///      Edition), Chapter 10
+  /// @see Satellite Orbits, Models, Methods, Applications, O. Montenbruck, E. 
+  ///      Gill, Chapter 3.5
+  Vector3 drag_accel(const Vector3 &r, const Vector3 &v, double Area,
+                        double mass, double CD, double atmdens) noexcept;
 }// dso
 
 #endif
---
+++ src/satellite/drag.cpp
@@ -0,0 +1,17 @@
+#include "satellite.hpp"
+
+using dso::Vector3;
+
+Vector3 dso::drag_accel(const Vector3 &rsat, const Vector3 &vsat, double Area,
+                        double CD, double mass, double atmdens) noexcept {
+  // atmospheric "velocity", due to rotation with the earth, Vatm = ω x r
+  const Vector3 v_atm{
+      {-rsat.y() * iers2010::OmegaEarth, -rsat.x() * iers2010::OmegaEarth}};
+
+  // spacecraft velocity relative to the atmosphere
+  const Vector3 v_rel = vsat - v_atm;
+  const double v = v_rel.norm();
+
+  // drag force acceleration
+  return v_rel * (-0.5e0 * v * atmdens * CD * (Area / mass));
+}
