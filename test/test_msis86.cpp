6949760bec41dcd8339515345fbbf2de429561b9
xanthos <xanthos.papanikolaou@gmail.com>
2022-02-23 02:17:32 +0200

    fixed msis86

--- test/test_msis86.cpp
+++ test/test_msis86.cpp
@@ -1,5 +1,6 @@
 #include "atmosphere.hpp"
 #include <cstdio>
+#include <cstring>
 
 using dso::air_density_models::msis86::msis86;
 
@@ -13,7 +14,8 @@ int main() {
   double f107a = 150.; // mean solar flux over 90 days
   double f107 = 150.;  // solar flux from previous day
   double ap[] = {4, 4, 4, 4, 4, 4, 4};
-  int switches[24] = {0};
+  int switches[24];
+  std::memset(switches, 1, sizeof(int) * 24);
   /*
   dayly Ap
   3 hr ap index for current time
@@ -31,88 +33,88 @@ int main() {
     printf("%.2E ", outd[i]);
   printf("\n    %.3E %.3E \n", outt[0], outt[1]);
 
-  day = 81;
-  msis86(day, sec, alt, glat, glong, stl, f107a, f107, switches, ap, outd,
-         outt);
-  for (int i = 0; i < 8; i++)
-    printf("%.2E ", outd[i]);
-  printf("\n    %.3E %.3E \n", outt[0], outt[1]);
+  //day = 81;
+  //msis86(day, sec, alt, glat, glong, stl, f107a, f107, switches, ap, outd,
+  //       outt);
+  //for (int i = 0; i < 8; i++)
+  //  printf("%.2E ", outd[i]);
+  //printf("\n    %.3E %.3E \n", outt[0], outt[1]);
 
-  day = 172;
-  sec = 75000.;
-  msis86(day, sec, alt, glat, glong, stl, f107a, f107, switches, ap, outd,
-         outt);
-  for (int i = 0; i < 8; i++)
-    printf("%.2E ", outd[i]);
-  printf("\n    %.3E %.3E \n", outt[0], outt[1]);
+  //day = 172;
+  //sec = 75000.;
+  //msis86(day, sec, alt, glat, glong, stl, f107a, f107, switches, ap, outd,
+  //       outt);
+  //for (int i = 0; i < 8; i++)
+  //  printf("%.2E ", outd[i]);
+  //printf("\n    %.3E %.3E \n", outt[0], outt[1]);
 
-  sec = 29000.;
-  alt = 100.;
-  ap[6] = 40;
-  msis86(day, sec, alt, glat, glong, stl, f107a, f107, switches, ap, outd,
-         outt);
-  for (int i = 0; i < 8; i++)
-    printf("%.2E ", outd[i]);
-  printf("\n    %.3E %.3E \n", outt[0], outt[1]);
+  //sec = 29000.;
+  //alt = 100.;
+  //ap[6] = 40;
+  //msis86(day, sec, alt, glat, glong, stl, f107a, f107, switches, ap, outd,
+  //       outt);
+  //for (int i = 0; i < 8; i++)
+  //  printf("%.2E ", outd[i]);
+  //printf("\n    %.3E %.3E \n", outt[0], outt[1]);
 
-  alt = 400.;
-  glat = 0.;
-  ap[5] = 40;
-  ap[6] = 0;
-  msis86(day, sec, alt, glat, glong, stl, f107a, f107, switches, ap, outd,
-         outt);
-  for (int i = 0; i < 8; i++)
-    printf("%.2E ", outd[i]);
-  printf("\n    %.3E %.3E \n", outt[0], outt[1]);
+  //alt = 400.;
+  //glat = 0.;
+  //ap[5] = 40;
+  //ap[6] = 0;
+  //msis86(day, sec, alt, glat, glong, stl, f107a, f107, switches, ap, outd,
+  //       outt);
+  //for (int i = 0; i < 8; i++)
+  //  printf("%.2E ", outd[i]);
+  //printf("\n    %.3E %.3E \n", outt[0], outt[1]);
 
-  glat = 60.;
-  glong = 0.;
-  ap[4] = 40;
-  ap[5] = 0;
-  msis86(day, sec, alt, glat, glong, stl, f107a, f107, switches, ap, outd,
-         outt);
-  for (int i = 0; i < 8; i++)
-    printf("%.2E ", outd[i]);
-  printf("\n    %.3E %.3E \n", outt[0], outt[1]);
+  //glat = 60.;
+  //glong = 0.;
+  //ap[4] = 40;
+  //ap[5] = 0;
+  //msis86(day, sec, alt, glat, glong, stl, f107a, f107, switches, ap, outd,
+  //       outt);
+  //for (int i = 0; i < 8; i++)
+  //  printf("%.2E ", outd[i]);
+  //printf("\n    %.3E %.3E \n", outt[0], outt[1]);
 
-  glong = -70.;
-  stl = 4.;
-  ap[3] = 40;
-  ap[4] = 0;
-  msis86(day, sec, alt, glat, glong, stl, f107a, f107, switches, ap, outd,
-         outt);
-  for (int i = 0; i < 8; i++)
-    printf("%.2E ", outd[i]);
-  printf("\n    %.3E %.3E \n", outt[0], outt[1]);
+  //glong = -70.;
+  //stl = 4.;
+  //ap[3] = 40;
+  //ap[4] = 0;
+  //msis86(day, sec, alt, glat, glong, stl, f107a, f107, switches, ap, outd,
+  //       outt);
+  //for (int i = 0; i < 8; i++)
+  //  printf("%.2E ", outd[i]);
+  //printf("\n    %.3E %.3E \n", outt[0], outt[1]);
 
-  stl = 16.;
-  f107a = 70.;
-  ap[2] = 40;
-  ap[3] = 0;
-  msis86(day, sec, alt, glat, glong, stl, f107a, f107, switches, ap, outd,
-         outt);
-  for (int i = 0; i < 8; i++)
-    printf("%.2E ", outd[i]);
-  printf("\n    %.3E %.3E \n", outt[0], outt[1]);
+  //stl = 16.;
+  //f107a = 70.;
+  //ap[2] = 40;
+  //ap[3] = 0;
+  //msis86(day, sec, alt, glat, glong, stl, f107a, f107, switches, ap, outd,
+  //       outt);
+  //for (int i = 0; i < 8; i++)
+  //  printf("%.2E ", outd[i]);
+  //printf("\n    %.3E %.3E \n", outt[0], outt[1]);
 
-  f107a = 150.;
-  f107 = 180.;
-  ap[1] = 40;
-  ap[2] = 0;
-  msis86(day, sec, alt, glat, glong, stl, f107a, f107, switches, ap, outd,
-         outt);
-  for (int i = 0; i < 8; i++)
-    printf("%.2E ", outd[i]);
-  printf("\n    %.3E %.3E \n", outt[0], outt[1]);
+  //f107a = 150.;
+  //f107 = 180.;
+  //ap[1] = 40;
+  //ap[2] = 0;
+  //msis86(day, sec, alt, glat, glong, stl, f107a, f107, switches, ap, outd,
+  //       outt);
+  //for (int i = 0; i < 8; i++)
+  //  printf("%.2E ", outd[i]);
+  //printf("\n    %.3E %.3E \n", outt[0], outt[1]);
 
-  f107 = 150.;
-  ap[0] = 40;
-  ap[1] = 0;
-  msis86(day, sec, alt, glat, glong, stl, f107a, f107, switches, ap, outd,
-         outt);
-  for (int i = 0; i < 8; i++)
-    printf("%.2E ", outd[i]);
-  printf("\n    %.3E %.3E \n", outt[0], outt[1]);
+  //f107 = 150.;
+  //ap[0] = 40;
+  //ap[1] = 0;
+  //msis86(day, sec, alt, glat, glong, stl, f107a, f107, switches, ap, outd,
+  //       outt);
+  //for (int i = 0; i < 8; i++)
+  //  printf("%.2E ", outd[i]);
+  //printf("\n    %.3E %.3E \n", outt[0], outt[1]);
 
   return 0;
 }
