e2892744e1d6ab851305690c19df26df813b9b24
xanthos <xanthos.papanikolaou@gmail.com>
2022-12-19 23:59:55 +0200

    minor

--- src/iers/solid_earth_tide_step1.cpp
+++ src/iers/solid_earth_tide_step1.cpp
@@ -40,11 +40,15 @@ int dso::SolidEarthTide::solid_earth_tide_step1(
   const double GM = cs._GM;
 
   const double RRm = Re / Rmoon;
-  const double RRm3 = RRm * RRm * RRm;
   const double RRs = Re / Rsun;
-  const double RRs3 = RRs * RRs * RRs;
-  const double GMme = GM_moon / GM;
-  const double GMse = GM_sun / GM;
+  // do not use this factor, scale results later on
+  const double RRm3 = 1e0; // RRm * RRm * RRm;
+  // do not use this factor, scale results later on
+  const double RRs3 = 1e0; // RRs * RRs * RRs;
+  // do not use this factor, scale results later on
+  const double GMme = 1e0; // GM_moon / GM;
+  // do not use this factor, scale results later on
+  const double GMse = 1e0; // GM_sun / GM;
   const double sml = std::sin(mlon);
   const double ssl = std::sin(slon);
   const double cml = std::cos(mlon);
@@ -54,102 +58,101 @@ int dso::SolidEarthTide::solid_earth_tide_step1(
   const double c2ml = 2e0 * cml * cml - 1e0; // std::cos(2e0*mlon);
   const double c2sl = 2e0 * csl * csl - 1e0; // std::cos(2e0*slon);
 
-  // TODO probably need to factor out GMme and RRm3
-
   // n = 2, m = 0
   double fac = (LoveK[0].knm) / (LoveK[0].n * 2 + 1);
   const double dc20_moon = fac * GMme * RRm3 * PM(2, 0);
-  const double dc20_sun  = fac * GMse * RRs3 * PS(2, 0);
-  printf(">>note, Moon: dc20 = %.15e = %.15e * %.15e * %.15e * %.15e\n", dc20_moon, fac, GMme, RRm3, PM(2, 0));
-  printf(">>note, Sun : dc20 = %.15e\n", dc20_sun);
+  const double dc20_sun = fac * GMse * RRs3 * PS(2, 0);
 
   // n = 2, m = 1
   fac = (LoveK[1].knm) / (LoveK[1].n * 2 + 1);
   const double dc21_moon = fac * GMme * RRm3 * PM(2, 1) * cml;
   const double ds21_moon = fac * GMme * RRm3 * PM(2, 1) * sml;
-  const double dc21_sun =  fac * GMse * RRs3 * PS(2, 1) * csl;
-  const double ds21_sun =  fac * GMse * RRs3 * PS(2, 1) * ssl;
+  const double dc21_sun = fac * GMse * RRs3 * PS(2, 1) * csl;
+  const double ds21_sun = fac * GMse * RRs3 * PS(2, 1) * ssl;
 
   // n = 2, m = 2
   fac = (LoveK[2].knm) / (LoveK[2].n * 2 + 1);
   const double dc22_moon = fac * GMme * RRm3 * PM(2, 2) * c2ml;
   const double ds22_moon = fac * GMme * RRm3 * PM(2, 2) * s2ml;
-  const double dc22_sun =  fac * GMse * RRs3 * PS(2, 2) * c2sl;
-  const double ds22_sun =  fac * GMse * RRs3 * PS(2, 2) * s2sl;
+  const double dc22_sun = fac * GMse * RRs3 * PS(2, 2) * c2sl;
+  const double ds22_sun = fac * GMse * RRs3 * PS(2, 2) * s2sl;
 
   // n = 3, m = 0
   const double RRm4 = RRm3 * RRm;
   const double RRs4 = RRs3 * RRs;
   fac = (LoveK[3].knm) / (LoveK[3].n * 2 + 1);
   const double dc30_moon = fac * GMme * RRm4 * PM(3, 0);
-  const double dc30_sun  = fac * GMse * RRs4 * PS(3, 0);
+  const double dc30_sun = fac * GMse * RRs4 * PS(3, 0);
 
   // n = 3, m = 1
   fac = (LoveK[4].knm) / (LoveK[4].n * 2 + 1);
   const double dc31_moon = fac * GMme * RRm4 * PM(3, 1) * cml;
   const double ds31_moon = fac * GMme * RRm4 * PM(3, 1) * sml;
-  const double dc31_sun =  fac * GMse * RRs4 * PS(3, 1) * csl;
-  const double ds31_sun =  fac * GMse * RRs4 * PS(3, 1) * ssl;
+  const double dc31_sun = fac * GMse * RRs4 * PS(3, 1) * csl;
+  const double ds31_sun = fac * GMse * RRs4 * PS(3, 1) * ssl;
 
   // n = 3, m = 2
   fac = (LoveK[5].knm) / (LoveK[5].n * 2 + 1);
   const double dc32_moon = fac * GMme * RRm4 * PM(3, 2) * c2ml;
   const double ds32_moon = fac * GMme * RRm4 * PM(3, 2) * s2ml;
-  const double dc32_sun =  fac * GMse * RRs4 * PS(3, 2) * c2sl;
-  const double ds32_sun =  fac * GMse * RRs4 * PS(3, 2) * s2sl;
+  const double dc32_sun = fac * GMse * RRs4 * PS(3, 2) * c2sl;
+  const double ds32_sun = fac * GMse * RRs4 * PS(3, 2) * s2sl;
 
   // n = 3, m = 3
   fac = (LoveK[6].knm) / (LoveK[6].n * 2 + 1);
-  const double dc33_moon =
-      fac * GMme * RRm4 * PM(3, 3) * std::cos(3e0 * mlon);
-  const double ds33_moon =
-      fac * GMme * RRm4 * PM(3, 3) * std::sin(3e0 * mlon);
+  const double dc33_moon = fac * GMme * RRm4 * PM(3, 3) * std::cos(3e0 * mlon);
+  const double ds33_moon = fac * GMme * RRm4 * PM(3, 3) * std::sin(3e0 * mlon);
   const double dc33_sun = fac * GMse * RRs4 * PS(3, 3) * std::cos(3e0 * slon);
   const double ds33_sun = fac * GMse * RRs4 * PS(3, 3) * std::sin(3e0 * slon);
 
   // n = 4, m = 0
   fac = (LoveK[7].knm) / 5;
   const double dc40_moon = fac * GMme * RRm3 * PM(2, 0);
-  const double dc40_sun =  fac * GMse * RRs3 * PS(2, 0);
+  const double dc40_sun = fac * GMse * RRs3 * PS(2, 0);
 
   // n = 4, m = 1
   fac = (LoveK[8].knm) / 5e0;
   const double dc41_moon = fac * GMme * RRm3 * PM(2, 1) * cml;
   const double ds41_moon = fac * GMme * RRm3 * PM(2, 1) * sml;
-  const double dc41_sun =  fac * GMse * RRs3 * PS(2, 1) * csl;
-  const double ds41_sun =  fac * GMse * RRs3 * PS(2, 1) * ssl;
+  const double dc41_sun = fac * GMse * RRs3 * PS(2, 1) * csl;
+  const double ds41_sun = fac * GMse * RRs3 * PS(2, 1) * ssl;
 
   // n = 4, m = 2
   fac = (LoveK[9].knm) / 5e0;
   const double dc42_moon = fac * GMme * RRm3 * PM(2, 2) * c2ml;
   const double ds42_moon = fac * GMme * RRm3 * PM(2, 2) * s2ml;
-  const double dc42_sun =  fac * GMse * RRs3 * PS(2, 2) * c2sl;
-  const double ds42_sun =  fac * GMse * RRs3 * PS(2, 2) * s2sl;
+  const double dc42_sun = fac * GMse * RRs3 * PS(2, 2) * c2sl;
+  const double ds42_sun = fac * GMse * RRs3 * PS(2, 2) * s2sl;
+
+  const double ReRm3 = RRm * RRm * RRm;
+  const double ReRs3 = RRs * RRs * RRs;
+  const double GmMGmE = GM_moon / GM;
+  const double GmSGmE = GM_sun / GM;
 
   // assign corrections
   std::fill(std::begin(dC), std::end(dC), 0e0);
-  dC[0] = dc20_moon + dc20_sun;
-  dC[1] = dc21_moon + dc21_sun;
-  dC[2] = dc22_moon + dc22_sun;
-  dC[3] = dc30_moon + dc30_sun;
-  dC[4] = dc31_moon + dc31_sun;
-  dC[5] = dc32_moon + dc32_sun;
-  dC[6] = dc33_moon + dc33_sun;
-  dC[7] = dc40_moon + dc40_sun;
-  dC[8] = dc41_moon + dc41_sun;
-  dC[9] = dc42_moon + dc42_sun;
+  dC[0] = dc20_moon * (GmMGmE * ReRm3) + dc20_sun * (GmSGmE * ReRs3);
+  dC[1] = dc21_moon * (GmMGmE * ReRm3) + dc21_sun * (GmSGmE * ReRs3);
+  dC[2] = dc22_moon * (GmMGmE * ReRm3) + dc22_sun * (GmSGmE * ReRs3);
+  dC[3] = dc30_moon * (GmMGmE * ReRm3) + dc30_sun * (GmSGmE * ReRs3);
+  dC[4] = dc31_moon * (GmMGmE * ReRm3) + dc31_sun * (GmSGmE * ReRs3);
+  dC[5] = dc32_moon * (GmMGmE * ReRm3) + dc32_sun * (GmSGmE * ReRs3);
+  dC[6] = dc33_moon * (GmMGmE * ReRm3) + dc33_sun * (GmSGmE * ReRs3);
+  dC[7] = dc40_moon * (GmMGmE * ReRm3) + dc40_sun * (GmSGmE * ReRs3);
+  dC[8] = dc41_moon * (GmMGmE * ReRm3) + dc41_sun * (GmSGmE * ReRs3);
+  dC[9] = dc42_moon * (GmMGmE * ReRm3) + dc42_sun * (GmSGmE * ReRs3);
 
   std::fill(std::begin(dS), std::end(dS), 0e0);
   // dS[0] = 0e0;
-  dS[1] = ds21_moon + ds21_sun;
-  dS[2] = ds22_moon + ds22_sun;
+  dS[1] = ds21_moon * (GmMGmE * ReRm3) + ds21_sun * (GmSGmE * ReRs3);
+  dS[2] = ds22_moon * (GmMGmE * ReRm3) + ds22_sun * (GmSGmE * ReRs3);
   // dS[3] = 0e0
-  dS[4] = ds31_moon + ds31_sun;
-  dS[5] = ds32_moon + ds32_sun;
-  dS[6] = ds33_moon + ds33_sun;
+  dS[4] = ds31_moon * (GmMGmE * ReRm3) + ds31_sun * (GmSGmE * ReRs3);
+  dS[5] = ds32_moon * (GmMGmE * ReRm3) + ds32_sun * (GmSGmE * ReRs3);
+  dS[6] = ds33_moon * (GmMGmE * ReRm3) + ds33_sun * (GmSGmE * ReRs3);
   // dS[7] =  0e0
-  dS[8] = ds41_moon + ds41_sun;
-  dS[9] = ds42_moon + ds42_sun;
+  dS[8] = ds41_moon * (GmMGmE * ReRm3) + ds41_sun * (GmSGmE * ReRs3);
+  dS[9] = ds42_moon * (GmMGmE * ReRm3) + ds42_sun * (GmSGmE * ReRs3);
 
   return 0;
 }
--- src/sh_cunningham_normalized_gradient.cpp
+++ src/sh_cunningham_normalized_gradient.cpp
@@ -3,30 +3,28 @@
 #include "geodesy/geodesy.hpp"
 #include "harmonic_coeffs.hpp"
 #include <array>
+#include <cassert>
 #include <cmath>
 #ifdef DEBUG
 #include <cassert>
 #include <cfenv>
 #include <limits>
-//#  pragma STDC_FENV_ACCESS on
 #endif
 
 namespace {
 struct KahanSum {
   double sum{0e0};
-  double c{0e0}; //error
+  double c{0e0}; // error
   void operator+(double val) noexcept {
     const double y = val - c;
     const double t = sum + y;
-    c = (t-sum) - y;
+    c = (t - sum) - y;
     sum = t;
   }
-  double operator()() const noexcept {
-    return sum;
-  }
-};// KahanSum
+  double operator()() const noexcept { return sum; }
+}; // KahanSum
 
-// compile-time factors
+// compile-time factors for Normalized ALFs
 template <int N> struct NormalizedLegendreFactors {
   std::array<double, N *(N + 1) / 2> fac1{};
   std::array<double, N *(N + 1) / 2> fac2{};
@@ -56,97 +54,99 @@ template <int N> struct NormalizedLegendreFactors {
     // factors for the recursion ( (2n+1)/2n )^(1/2)
     f1(1, 1) = std::sqrt(3e0);
     for (int n = 2; n < N; n++)
-      f1(n, n) = std::sqrt(static_cast<double>(2 * n + 1) /
-                           static_cast<double>(2 * n));
+      f1(n, n) = std::sqrt((2 * n + 1) / (2e0 * n));
 
     // factors for the recursion
     for (int m = 0; m < N; m++) {
       for (int n = m + 1; n < N; n++) {
-        // --- a little easier .....
-        // double f = static_cast<double>(2 * n + 1) /
-        //           static_cast<double>((n + m) * (n - m));
-        //// f1_nm = B_nm
-        // f1(n, m) = std::sqrt(f * (2 * n - 1));
-        //// f2_nm = B_nm / Bn-1m
-        // f2(n, m) =
-        //     - std::sqrt(f * (n - m - 1e0) * (n + m - 1e0) / (2e0 * n - 3e0));
-        //  -- alternative ... keep integers as late as possible
-        const long n1 = 2 * n + 1;
-        const long n2 = (n + m) * (n - m);
-        const long f11 = n1 * (2 * n - 1);
-        const long f12 = n2;
-        f1(n, m) = std::sqrt((double)f11 / (double)f12);
-
-        const long f21 = n1 * (n - m - 1) * (n + m - 1);
-        const long f22 = n2 * (2 * n - 3);
-        f2(n, m) = -std::sqrt((double)f21 / (double)f22);
+        const double f = (2 * n + 1) / ((n + m) * (n - m));
+        // f1_nm = B_nm
+        f1(n, m) = std::sqrt(f * (2 * n - 1));
+        // f2_nm = B_nm / Bn-1m
+        f2(n, m) = -std::sqrt(f * (n - m - 1) * (n + m - 1) / (2 * n - 3));
       }
     }
 
     // factors for acceleration
     for (int n = 0; n < N; n++)
-      fac3[n] = std::sqrt((double)(2 * n + 1) / (double)(2 * n + 3));
+      fac3[n] = std::sqrt((double)(2 * n + 1) / (2 * n + 3));
   }
 }; // SHFactors
 } // namespace
 
-class Factor {
-public:
-  double f1(int n, int m) const noexcept {
-#ifdef DEBUG
-    assert(n > 0 && m <= n);
-#endif
-    if (n == m)
-      return (n == 1) ? std::sqrt(3.0) : std::sqrt((2. * n + 1.) / (2. * n));
-    else
-      return std::sqrt((2. * n + 1.) / static_cast<double>((n + m) * (n - m)) *
-                       (2. * n - 1.));
-  }
-  double f2(int n, int m) const noexcept {
-    return -std::sqrt((2. * n + 1.) / static_cast<double>((n + m) * (n - m)) *
-                      (n - m - 1.) * (n + m - 1.) / (2. * n - 3.));
-  }
-};
+// class Factor {
+// public:
+//   double f1(int n, int m) const noexcept {
+//#ifdef DEBUG
+//     assert(n > 0 && m <= n);
+//#endif
+//     if (n == m)
+//       return (n == 1) ? std::sqrt(3.0) : std::sqrt((2. * n + 1.) / (2. * n));
+//     else
+//       return std::sqrt((2. * n + 1.) / static_cast<double>((n + m) * (n - m))
+//       *
+//                        (2. * n - 1.));
+//   }
+//   double f2(int n, int m) const noexcept {
+//     return -std::sqrt((2. * n + 1.) / static_cast<double>((n + m) * (n - m))
+//     *
+//                       (n - m - 1.) * (n + m - 1.) / (2. * n - 3.));
+//   }
+// };
 
-static const NormalizedLegendreFactors<125> F;
+// Max size for ALF factors; if degree is mote than this (-2), then it must
+// be augmented. For now, OK
+constexpr const int MAX_SIZE_FOR_ALF_FACTORS = 125;
+
+// Factors up to degree/order 124
+static const NormalizedLegendreFactors<MAX_SIZE_FOR_ALF_FACTORS> F;
 
 /*            | dx/dx dx/dy dx/dz |
  * gradient = | dy/dx dy/dy dy/dz |
  *            | dz/dx dz/dy dz/dz |
  */
-int gravacc3_impl(const dso::HarmonicCoeffs &cs,
-                   const Eigen::Matrix<double, 3, 1> &p, int degree, double Re,
-                   double GM, Eigen::Matrix<double, 3, 1> &acc,
-                   Eigen::Matrix<double, 3, 3> &gradient, 
-                   dso::Mat2D<dso::MatrixStorageType::LwTriangularColWise> &W, 
-                   dso::Mat2D<dso::MatrixStorageType::LwTriangularColWise> &M) noexcept {
+int gravacc3_impl(
+    const dso::HarmonicCoeffs &cs, const Eigen::Matrix<double, 3, 1> &p,
+    int degree, double Re, double GM, Eigen::Matrix<double, 3, 1> &acc,
+    Eigen::Matrix<double, 3, 3> &gradient,
+    dso::Mat2D<dso::MatrixStorageType::LwTriangularColWise> &W,
+    dso::Mat2D<dso::MatrixStorageType::LwTriangularColWise> &M) noexcept {
+
+  if (degree > MAX_SIZE_FOR_ALF_FACTORS - 3) {
+    fprintf(stderr,
+            "[ERROR] (Static) Size for NormalizedLegendreFactors must be "
+            "augmented to perform computation (traceback: %s)\n",
+            __func__);
+    assert(false);
+  }
 
   const int lp_degree = degree + 2; // aka, [0,....degree+1]
-  
+
   W.fill_with(0e0);
   M.fill_with(0e0);
 
-  // Associated Legendre Polynomials
-  {
+  { // (kinda) Associated Legendre Polynomials M and W
+    // note that since we are going to compute derivatives (of the gtavity
+    // potential, we will compute M and W up to degree n+2,m+2
     const Eigen::Matrix<double, 3, 1> r = p / Re;
-    const double rr = std::pow(1 / r.norm(), 2);
+    const double rr = std::pow(1e0 / r.norm(), 2);
     const double x = r.x() * rr;
     const double y = r.y() * rr;
     const double z = r.z() * rr;
 
-    // start ALF iteration
+    // start ALF iteration (can use scaling according to Holmes et al, 2002)
     const int __use_factor = true;
     if (__use_factor)
       M(0, 0) = 1e280 / r.norm();
     else
       M(0, 0) = 1e0 / r.norm();
-    M(1, 0) = std::sqrt(3e0) * z * M(0, 0); // F.f1(1, 0) * z * M(0, 0);
+    M(1, 0) = std::sqrt(3e0) * z * M(0, 0);
 
-    // first fill column 0;  note that W(n,0) = 0 (already set)
+    // first fill m=0 terms; note that W(n,0) = 0 (already set)
     for (int n = 2; n <= lp_degree; n++)
       M(n, 0) = F.f1(n, 0) * z * M(n - 1, 0) + F.f2(n, 0) * rr * M(n - 2, 0);
 
-    // fill all columns of m >= 1
+    // fill all elements for order m >= 1
     for (int m = 1; m < lp_degree; m++) {
       // M(m,m) and W(m,m) aka, diagonal
       M(m, m) = F.f1(m, m) * (x * M(m - 1, m - 1) - y * W(m - 1, m - 1));
@@ -163,8 +163,7 @@ int gravacc3_impl(const dso::HarmonicCoeffs &cs,
       }
     }
 
-    // well, we've left the lst term uncomputed
-    {
+    { // well, we've left the lst term uncomputed
       const int m = lp_degree;
       M(m, m) = F.f1(m, m) * (x * M(m - 1, m - 1) - y * W(m - 1, m - 1));
       W(m, m) = F.f1(m, m) * (y * M(m - 1, m - 1) + x * W(m - 1, m - 1));
@@ -174,40 +173,21 @@ int gravacc3_impl(const dso::HarmonicCoeffs &cs,
       M.multiply(1e-280);
       W.multiply(1e-280);
     }
-  } // end computing Legendre
-  /* groops-like ....
-  for (int m=1; m<=lp_degree; m++) {
-    M(m, m) = F.f1(m, m) * (x * M(m - 1, m - 1) - y * W(m - 1, m - 1));
-    W(m, m) = F.f1(m, m) * (y * M(m - 1, m - 1) + x * W(m - 1, m - 1));
-  }
-  for (int m=1; m<=lp_degree; m++) {
-    M(m, m-1) = F.f1(m, m-1) * z * M(m-1, m-1);
-    W(m, m-1) = F.f1(m, m-1) * z * W(m-1, m-1);
-  }
-  for (int m=0; m<lp_degree-1; m++) {
-    for (int n=m+2; n<=lp_degree; n++) {
-      M(n, m) = F.f1(n, m) * z * M(n - 1, m) + F.f2(n, m) * rr * M(n - 2, m);
-      W(n, m) = F.f1(n, m) * z * W(n - 1, m) + F.f2(n, m) * rr * W(n - 2, m);
-    }
-  }
-  M.multiply(1e-280);
-  W.multiply(1e-280);
-  }*/
+  } // end computing ALF
 
   // acceleration and gradient in cartesian components
   acc = Eigen::Matrix<double, 3, 1>::Zero();
-  gradient = Eigen::Matrix<double,3,3>::Zero();
+  gradient = Eigen::Matrix<double, 3, 3>::Zero();
   [[maybe_unused]] const int minDegree = 1;
 #ifdef KAHAN_SUM
-    KahanSum axs,ays,azs;
+  KahanSum axs, ays, azs;
 #endif
 
-  // start from smaller terms. note that for degrees m=0,1, we are using 
+  // start from smaller terms. note that for degrees m=0,1, we are using
   // seperate loops
   for (int m = degree; m >= 2; --m) {
     for (int n = degree; n >= m; --n) {
-      // acceleration
-      {
+      { // acceleration
         const double wm1 =
             std::sqrt(static_cast<double>(n - m + 1) * (n - m + 2));
         const double wm0 =
@@ -227,15 +207,14 @@ int gravacc3_impl(const dso::HarmonicCoeffs &cs,
         const double az = cs.C(n, m) * (-2 * Cm0) + cs.S(n, m) * (-2 * Sm0);
 
 #ifdef KAHAN_SUM
-  axs + ax*std::sqrt((2e0 * n + 1e0) / (2e0 * n + 3e0));
-  ays + ay*std::sqrt((2e0 * n + 1e0) / (2e0 * n + 3e0));
-  azs + az*std::sqrt((2e0 * n + 1e0) / (2e0 * n + 3e0));
+        axs + ax *std::sqrt((2e0 * n + 1e0) / (2e0 * n + 3e0));
+        ays + ay *std::sqrt((2e0 * n + 1e0) / (2e0 * n + 3e0));
+        azs + az *std::sqrt((2e0 * n + 1e0) / (2e0 * n + 3e0));
 #endif
         acc += Eigen::Matrix<double, 3, 1>(ax, ay, az) *
                std::sqrt((2e0 * n + 1e0) / (2e0 * n + 3e0));
       }
-      // derivative of acceleration
-      {
+      { // derivative of acceleration
         const double wm2 = std::sqrt(static_cast<double>(n - m + 1) *
                                      (n - m + 2) * (n - m + 3) * (n - m + 4)) *
                            ((m == 2) ? std::sqrt(2.0) : 1.0);
@@ -276,47 +255,45 @@ int gravacc3_impl(const dso::HarmonicCoeffs &cs,
                     std::sqrt((2e0 * n + 1e0) / (2e0 * n + 5e0));
       }
     } // loop over n
-  } // loop over m
+  }   // loop over m
 
   // order m = 1
   for (int n = degree; n >= 1; --n) { // begin summation from smaller terms
     const int m = 1;
-    // acceleration
-    {
-        // only difference with the generalized formula (aka for random n,m)
-        // is in wm1
-        const double wm1 =
-            std::sqrt(static_cast<double>(n - m + 1) * (n - m + 2)) *
-            std::sqrt(2e0);
-        const double wm0 =
-            std::sqrt(static_cast<double>(n - m + 1) * (n + m + 1));
-        const double wp1 =
-            std::sqrt(static_cast<double>(n + m + 1) * (n + m + 2));
+    { // acceleration
+      // only difference with the generalized formula (aka for random n,m)
+      // is in wm1
+      const double wm1 =
+          std::sqrt(static_cast<double>(n - m + 1) * (n - m + 2)) *
+          std::sqrt(2e0);
+      const double wm0 =
+          std::sqrt(static_cast<double>(n - m + 1) * (n + m + 1));
+      const double wp1 =
+          std::sqrt(static_cast<double>(n + m + 1) * (n + m + 2));
 
-        const double Cm1 = wm1 * M(n + 1, m - 1);
-        const double Sm1 = wm1 * W(n + 1, m - 1);
-        const double Cm0 = wm0 * M(n + 1, m);
-        const double Sm0 = wm0 * W(n + 1, m);
-        const double Cp1 = wp1 * M(n + 1, m + 1);
-        const double Sp1 = wp1 * W(n + 1, m + 1);
+      const double Cm1 = wm1 * M(n + 1, m - 1);
+      const double Sm1 = wm1 * W(n + 1, m - 1);
+      const double Cm0 = wm0 * M(n + 1, m);
+      const double Sm0 = wm0 * W(n + 1, m);
+      const double Cp1 = wp1 * M(n + 1, m + 1);
+      const double Sp1 = wp1 * W(n + 1, m + 1);
 
-        const double ax = cs.C(n, m) * (Cm1 - Cp1) + cs.S(n, m) * (Sm1 - Sp1);
-        const double ay = cs.C(n, m) * (-Sm1 - Sp1) + cs.S(n, m) * (Cm1 + Cp1);
-        const double az = cs.C(n, m) * (-2e0 * Cm0) + cs.S(n, m) * (-2 * Sm0);
+      const double ax = cs.C(n, m) * (Cm1 - Cp1) + cs.S(n, m) * (Sm1 - Sp1);
+      const double ay = cs.C(n, m) * (-Sm1 - Sp1) + cs.S(n, m) * (Cm1 + Cp1);
+      const double az = cs.C(n, m) * (-2e0 * Cm0) + cs.S(n, m) * (-2 * Sm0);
 
 #ifdef KAHAN_SUM
-  axs + ax*std::sqrt((2e0 * n + 1e0) / (2e0 * n + 3e0));
-  ays + ay*std::sqrt((2e0 * n + 1e0) / (2e0 * n + 3e0));
-  azs + az*std::sqrt((2e0 * n + 1e0) / (2e0 * n + 3e0));
+      axs + ax *std::sqrt((2e0 * n + 1e0) / (2e0 * n + 3e0));
+      ays + ay *std::sqrt((2e0 * n + 1e0) / (2e0 * n + 3e0));
+      azs + az *std::sqrt((2e0 * n + 1e0) / (2e0 * n + 3e0));
 #endif
-        acc += Eigen::Matrix<double, 3, 1>(ax, ay, az) *
-               std::sqrt((2e0 * n + 1e0) / (2e0 * n + 3e0));
+      acc += Eigen::Matrix<double, 3, 1>(ax, ay, az) *
+             std::sqrt((2e0 * n + 1e0) / (2e0 * n + 3e0));
     }
-    // derivative of acceleration
-    {
+    { // derivative of acceleration
       const double wm1 = std::sqrt(static_cast<double>(n - m + 1) *
                                    (n - m + 2) * (n - m + 3) * (n + m + 1)) *
-                         std::sqrt(2.0);
+                         std::sqrt(2e0);
       const double wm0 = std::sqrt(static_cast<double>(n - m + 1) *
                                    (n - m + 2) * (n + m + 1) * (n + m + 2));
       const double wp1 = std::sqrt(static_cast<double>(n - m + 1) *
@@ -350,13 +327,12 @@ int gravacc3_impl(const dso::HarmonicCoeffs &cs,
                   std::sqrt((2e0 * n + 1e0) / (2e0 * n + 5e0));
     }
   }
-  
+
   // order m = 0
   for (int n = degree; n >= minDegree;
        --n) { // begin summation from smaller terms
-    [[maybe_unused]]const int m = 0;
-    // acceleration
-    {
+    [[maybe_unused]] const int m = 0;
+    { // acceleration
       double wm0 = std::sqrt(static_cast<double>(n + 1) * (n + 1));
       double wp1 =
           std::sqrt(static_cast<double>(n + 1) * (n + 2)) / std::sqrt(2e0);
@@ -370,23 +346,22 @@ int gravacc3_impl(const dso::HarmonicCoeffs &cs,
       const double az = cs.C(n, 0) * (-2e0 * Cm0);
 
 #ifdef KAHAN_SUM
-  axs + ax*std::sqrt((2e0 * n + 1e0) / (2e0 * n + 3e0));
-  ays + ay*std::sqrt((2e0 * n + 1e0) / (2e0 * n + 3e0));
-  azs + az*std::sqrt((2e0 * n + 1e0) / (2e0 * n + 3e0));
+      axs + ax *std::sqrt((2e0 * n + 1e0) / (2e0 * n + 3e0));
+      ays + ay *std::sqrt((2e0 * n + 1e0) / (2e0 * n + 3e0));
+      azs + az *std::sqrt((2e0 * n + 1e0) / (2e0 * n + 3e0));
 #endif
       acc += Eigen::Matrix<double, 3, 1>(ax, ay, az) *
              std::sqrt((2e0 * n + 1.) / (2e0 * n + 3e0));
     }
-    // derivative of acceleration
-    {
+    { // derivative of acceleration
       const double wm0 =
           std::sqrt(static_cast<double>(n + 1) * (n + 2) * (n + 1) * (n + 2));
       const double wp1 =
           std::sqrt(static_cast<double>(n + 1) * (n + 1) * (n + 2) * (n + 3)) /
-          std::sqrt(2.0);
+          std::sqrt(2e0);
       const double wp2 =
           std::sqrt(static_cast<double>(n + 1) * (n + 2) * (n + 3) * (n + 4)) /
-          std::sqrt(2.0);
+          std::sqrt(2e0);
 
       const double Cm0 = wm0 * M(n + 2, 0);
       const double Cp1 = wp1 * M(n + 2, 1);
@@ -408,53 +383,14 @@ int gravacc3_impl(const dso::HarmonicCoeffs &cs,
     }
   }
 
-  /* groops-like ...
-  Eigen::Matrix<double,3,1> g, g_ges=Eigen::Matrix<double,3,1>::Zero();
-  for (int n = minDegree; n <= degree; n++) {
-    double wm0 = std::sqrt(static_cast<double>(n + 1) * (n + 1));
-    double wp1 =
-        std::sqrt(static_cast<double>(n + 1) * (n + 2)) / std::sqrt(2.0);
-
-    double Cm0 = wm0 * W(n + 1, 0);
-    double Cp1 = wp1 * W(n + 1, 1);
-    double Sp1 = wp1 * W(n + 1, 1);
-
-    g.x() = cs.C(n, 0) * (-2 * Cp1);
-    g.y() = cs.C(n, 0) * (-2 * Sp1);
-    g.z() = cs.C(n, 0) * (-2 * Cm0);
-
-    for (int m = 1; m <= n; m++) {
-      double wm1 = std::sqrt(static_cast<double>(n - m + 1) * (n - m + 2)) *
-                   ((m == 1) ? std::sqrt(2.0) : 1.0);
-      wm0 = std::sqrt(static_cast<double>(n - m + 1) * (n + m + 1));
-      wp1 = std::sqrt(static_cast<double>(n + m + 1) * (n + m + 2));
-
-      double Cm1 = wm1 * W(n + 1, m - 1);
-      double Sm1 = wm1 * W(n + 1, m - 1);
-      Cm0 = wm0 * W(n + 1, m);
-      double Sm0 = wm0 * W(n + 1, m);
-      Cp1 = wp1 * W(n + 1, m + 1);
-      Sp1 = wp1 * W(n + 1, m + 1);
-
-      g.x() += cs.C(n, m) * (Cm1 - Cp1) + cs.S(n, m) * (Sm1 - Sp1);
-      g.y() += cs.C(n, m) * (-Sm1 - Sp1) + cs.S(n, m) * (Cm1 + Cp1);
-      g.z() += cs.C(n, m) * (-2 * Cm0) + cs.S(n, m) * (-2 * Sm0);
-    }
-    g_ges += std::sqrt((2. * n + 1.) / (2. * n + 3.)) * g;
-  }
-
-  g_ges = GM/(2*Re*Re) * g_ges;
-  printf("# Acceleration diffs = %.15f %.15f %.15f\n", g_ges(0)-acc(0),  g_ges(1)-acc(1),  g_ges(2)-acc(2));
-  acc = g_ges;
-  */
-
   // scale ...
-  gradient *= GM/(4e0*Re*Re*Re);
-  acc *= GM/(2e0*Re*Re);
+  gradient *= GM / (4e0 * Re * Re * Re);
+  acc *= GM / (2e0 * Re * Re);
 #ifdef KAHAN_SUM
-  Eigen::Matrix<double,3,1> ks({axs(), ays(), azs()});
-  ks *= GM/(2e0*Re*Re);
-  printf("# Acceleration diffs = %.15f %.15f %.15f\n", ks(0)-acc(0),  ks(1)-acc(1),  ks(2)-acc(2));
+  Eigen::Matrix<double, 3, 1> ks({axs(), ays(), azs()});
+  ks *= GM / (2e0 * Re * Re);
+  printf("# Acceleration diffs = %.15f %.15f %.15f\n", ks(0) - acc(0),
+         ks(1) - acc(1), ks(2) - acc(2));
 #endif
 
   return 0;
@@ -473,12 +409,12 @@ int test::gravacc3(const dso::HarmonicCoeffs &cs,
   return gravacc3_impl(cs, p, degree, Re, GM, acc, gradient, W, M);
 }
 
-int test::gravacc3(const dso::HarmonicCoeffs &cs,
-                   const Eigen::Matrix<double, 3, 1> &p, int degree, double Re,
-                   double GM, Eigen::Matrix<double, 3, 1> &acc,
-                   Eigen::Matrix<double, 3, 3> &gradient, 
-                   dso::Mat2D<dso::MatrixStorageType::LwTriangularColWise> *W, 
-                   dso::Mat2D<dso::MatrixStorageType::LwTriangularColWise> *M) noexcept {
+int test::gravacc3(
+    const dso::HarmonicCoeffs &cs, const Eigen::Matrix<double, 3, 1> &p,
+    int degree, double Re, double GM, Eigen::Matrix<double, 3, 1> &acc,
+    Eigen::Matrix<double, 3, 3> &gradient,
+    dso::Mat2D<dso::MatrixStorageType::LwTriangularColWise> *W,
+    dso::Mat2D<dso::MatrixStorageType::LwTriangularColWise> *M) noexcept {
 
   const int lp_degree = degree + 2; // aka, [0,....degree+1]
   if (((W->rows() < lp_degree + 1) || (W->cols() < lp_degree + 1)) ||
@@ -492,4 +428,3 @@ int test::gravacc3(const dso::HarmonicCoeffs &cs,
 
   return gravacc3_impl(cs, p, degree, Re, GM, acc, gradient, *W, *M);
 }
-
